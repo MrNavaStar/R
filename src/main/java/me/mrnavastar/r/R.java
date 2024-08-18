510589d50940c66884081096411208b73ec87f25
MrNavaStar <Mr.NavaStar@gmail.com>
2024-08-17 23:54:30 -0600

    maven central time

--- src/main/java/me/mrnavastar/r/R.java
+++ src/main/java/me/mrnavastar/r/R.java
@@ -18,10 +18,16 @@ public class R {
         this.clazz = clazz;
     }
 
+    /**
+     * Create an instance of {@link R}. Can be used for static or non-static actions
+     */
     public static R of(Object instance) {
         return new R(instance);
     }
 
+    /**
+     * Create an instance of {@link R} that can only be used for static actions
+     */
     public static R of(Class<?> clazz) {
         return new R(clazz);
     }
@@ -54,6 +60,9 @@ public class R {
         return method;
     }
 
+    /**
+     * Get the value of a field. Can be private or static
+     */
     public <T> T get(String name, Class<T> type) {
         try {
             return type.cast(findField(clazz, name).get(instance));
@@ -62,6 +71,9 @@ public class R {
         }
     }
 
+    /**
+     * Set the value of a field. Can be private, final, or static
+     */
     public void set(String name, Object value) {
         try {
             findField(clazz, name).set(instance, value);
@@ -70,6 +82,9 @@ public class R {
         }
     }
 
+    /**
+     * Invoke a function with a return type
+     */
     public <T> T call(String name, Class<T> returnType, Object... args) {
         try {
             Class<?>[] classes = Arrays.stream(args).map(Object::getClass).toArray(Class[]::new);
@@ -81,11 +96,17 @@ public class R {
         }
     }
 
+    /**
+     * Invoke a function with no return type
+     */
     public R call(String name, Object... args) {
         call(name, null, args);
         return this;
     }
 
+    /**
+     * Get a list of the generic type params of a class
+     */
     public Class<?>[] generics() {
         Type generic = clazz.getGenericSuperclass();
         if (generic instanceof ParameterizedType) {
